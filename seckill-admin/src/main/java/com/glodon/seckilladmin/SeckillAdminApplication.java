b3fec5b5b66eb9162d9fa67520e7b300402d1dd8
王鹏鹏(10029595) <wangpp-b@glodon.com>
2019-08-12 11:14:56 +0800

    'firstcommit''

---
+++ seckill-admin/src/main/java/com/glodon/seckilladmin/SeckillAdminApplication.java
@@ -0,0 +1,13 @@
+package com.glodon.seckilladmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeckillAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeckillAdminApplication.class, args);
+	}
+
+}
---
+++ seckill-admin/src/test/java/com/glodon/seckilladmin/SeckillAdminApplicationTests.java
@@ -0,0 +1,16 @@
+package com.glodon.seckilladmin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SeckillAdminApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
