40b40b4a87ddfd6834a3944283f98d5175c1cbd0
魏笑甜(10029597) <weixt@glodon.com>
2019-08-12 12:36:33 +0800

    魏创建User实体类

---
+++ seckill-admin/src/main/java/com/glodon/seckilladmin/mapper/UserAdminMapper.java
@@ -0,0 +1,11 @@
+package com.glodon.seckilladmin.mapper;
+
+import com.glodon.seckilladmin.domain.UserAdmin;
+import org.springframework.stereotype.Repository;
+
+/**
+ * UserAdminMapper继承基类
+ */
+@Repository
+public interface UserAdminMapper extends MyBatisBaseDao<UserAdmin, Integer> {
+}
