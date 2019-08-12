30504c67e47967cfedaac68523c048789a2080a7
王鹏鹏(10029595) <wangpp-b@glodon.com>
2019-08-12 11:26:05 +0800

    王鹏鹏的首次配置

--- seckill-admin/src/main/java/com/glodon/seckilladmin/SeckillAdminApplication.java
+++ seckill-admin/src/main/java/com/glodon/seckilladmin/SeckillAdminApplication.java
@@ -1,9 +1,13 @@
 package com.glodon.seckilladmin;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan
+@MapperScan("com.glodon.seckilladmin.dao")
 public class SeckillAdminApplication {
 
 	public static void main(String[] args) {
