0a8f788b9c4c33feee8accc82caaddbc8de2a95e
王鹏鹏(10029595) <wangpp-b@glodon.com>
2019-08-12 11:41:15 +0800

    王鹏鹏的首次配置

---
+++ seckill-common/src/main/java/com/glodon/seckillcommon/SeckillCommonApplication.java
@@ -0,0 +1,13 @@
+package com.glodon.seckillcommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeckillCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeckillCommonApplication.class, args);
+	}
+
+}
